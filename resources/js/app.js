4c96eafd414ab15ef1d8dca893d288fdf2327542
Steve <steve.stewart2612@gmail.com>
2020-11-30 21:52:04 +0200

    Auth scaffolding added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.axios = require('axios');
 
-require('./bootstrap');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.withCredentials = true;
 
 window.Vue = require('vue');
 
